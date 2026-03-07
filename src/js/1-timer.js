2088eb34cb3863cf865938a245ec390b0d284944
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-03-08 00:25:37 +0200

    add task-1 timer full

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,93 @@
-console.log('timer initialized');
+import '../css/1-timer.css';
+import flatpickr from 'flatpickr';
+import iziToast from 'izitoast';
+import 'flatpickr/dist/flatpickr.min.css';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+let userSelectedDate = null;
+let timerInterval = null;
+
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+// ------- Форматування часу ----------
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+// -----------  converts in ms ------------
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// ---- для оновлення інтерфейсу таймера --------
+function updateTimerUI(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
+
+// ----- Ініціалізація функції flatpickr для вибору дати ----
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+    if (selectedDate <= now) {
+      userSelectedDate = null;
+      startBtn.disabled = true;
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(input, options);
+
+// ---------- Pressing the Start button -----------
+startBtn.addEventListener('click', () => {
+  if (!userSelectedDate) return;
+
+  startBtn.disabled = true;
+  input.disabled = true;
+
+  timerInterval = setInterval(() => {
+    const now = new Date();
+    const deltaTime = userSelectedDate - now;
+
+    if (deltaTime <= 0) {
+      clearInterval(timerInterval);
+      updateTimerUI(0);
+      input.disabled = false;
+      startBtn.disabled = true;
+      return;
+    }
+    updateTimerUI(deltaTime);
+  }, 1000);
+
+  updateTimerUI(userSelectedDate - new Date());
+});
