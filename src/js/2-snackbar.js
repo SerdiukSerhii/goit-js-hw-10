e7c89b7ce07c60f290215020529d3fcadf5ecb16
SerdiukSerhii <Serdyuk.logist@gmail.com>
2026-03-08 04:40:18 +0200

    add task-2 js

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,43 @@
-console.log('snackbar');
+import '../css/2-snackbar.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.js-form');
+
+function createPromise(delay, state) {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        res(delay);
+      } else {
+        rej(delay);
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  const promise = createPromise(delay, state);
+
+  promise
+    .then(delayValue => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delayValue}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    })
+    .catch(delayValue => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delayValue}ms`,
+        position: 'topRight',
+        icon: false,
+      });
+    });
+  form.reset();
+});
